bd94e9e02d380a618304f8d9c4dc8589966df1dc
Zekai Koç <zekaikoc@gmail.com>
2023-11-21 19:40:42 +0100

    seperate components created.

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,45 +1,10 @@
-import reactImg from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function Header() {
-   return (
-      <header>
-         <img src={reactImg} alt="Stylized atom" />
-         <h1>React Essentials</h1>
-         <p>
-            {
-               reactDescriptions[
-                  Math.floor(Math.random() * reactDescriptions.length)
-               ]
-            }{" "}
-            React concepts you will need for almost any app you are going to
-            build!
-         </p>
-      </header>
-   );
-}
-
-const CoreConcept = ({ title, description, image }) => {
-   return (
-      <li>
-         <img src={image} alt={title} />
-         <h3>{title}</h3>
-         <p>{description}</p>
-      </li>
-   );
-};
+import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcept";
 
 function App() {
    return (
       <div>
-         {/* {
-  for (let index = 0; index < CORE_CONCEPTS.length; index++) {
-    
-    console.log(i)
-  }
-}  */}
          <Header />
          <main>
             <section id="core-concepts">
---
+++ 01-starting-project/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import reactImg from "../assets/react-core-concepts.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+export default function Header() {
+   return (
+      <header>
+         <img src={reactImg} alt="Stylized atom" />
+         <h1>React Essentials</h1>
+         <p>
+            {
+               reactDescriptions[
+                  Math.floor(Math.random() * reactDescriptions.length)
+               ]
+            }{" "}
+            React concepts you will need for almost any app you are going to
+            build!
+         </p>
+      </header>
+   );
+}
