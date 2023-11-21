6b6d15780e0dd51a36f3290a97fab0d631e641e4
Zekai Koç <zekaikoc@gmail.com>
2023-11-21 17:49:58 +0100

    dynamic list element added.

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -20,13 +20,31 @@ function Header() {
    );
 }
 
+const CoreConcept = ({ title, description, image }) => {
+   return (
+      <li>
+         <img src={image} alt={title} />
+         <h3>{title}</h3>
+         <p>{description}</p>
+      </li>
+   );
+};
+
 function App() {
    return (
       <div>
-         {/* {Header()} */}
          <Header />
          <main>
-            <h2>Time to get started!</h2>
+            <section id="core-concepts">
+               <h2>Time to get started!</h2>
+               <ul>
+                  <CoreConcept
+                     title="Components"
+                     description="The core UI building block."
+                     image={reactImg}
+                  />
+               </ul>
+            </section>
          </main>
       </div>
    );
