bf95fcf949530ada20b54e6bb05d010034a76aa1
Zekai Koç <zekaikoc@gmail.com>
2023-11-24 15:35:47 +0100

    main board created.

--- 05_Essentials_Practice_TikTokGame/src/App.jsx
+++ 05_Essentials_Practice_TikTokGame/src/App.jsx
@@ -1,10 +1,24 @@
-
 function App() {
-  
-
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+   return (
+      <main>
+         <div id="game-container">
+            <ol id="players">
+               <li>
+                  <span className="player-name">
+                     Player 1<span className="player-symbol">X</span>
+                  </span>
+               </li>
+               <li>
+                  <span className="player-name">
+                     Player 2<span className="player-symbol">O</span>
+                  </span>
+               </li>
+            </ol>
+            game board
+         </div>
+         log
+      </main>
+   );
 }
 
-export default App
+export default App;
