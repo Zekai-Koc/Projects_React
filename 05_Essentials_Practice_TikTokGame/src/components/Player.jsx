fcc4b90c6090aab3318fc97ed4612b20d661e4a5
Zekai Koç <zekaikoc@gmail.com>
2023-11-25 15:18:31 +0100

    two way binding added.

--- 05_Essentials_Practice_TikTokGame/src/components/Player.jsx
+++ 05_Essentials_Practice_TikTokGame/src/components/Player.jsx
@@ -1,24 +1,36 @@
 import React, { useState } from "react";
 
-const Player = ({ name, symbol }) => {
+const Player = ({ initialName, symbol }) => {
+   const [playerName, setPlayerName] = useState(initialName);
    const [isEditing, setIsEditing] = useState(false);
 
    const handleEditClick = () => {
-      setIsEditing(!isEditing);
+      setIsEditing((editing) => !editing);
    };
 
-   let playerName = <span className="player-name">{name} </span>;
+   const handleChange = (e) => {
+      setPlayerName(e.target.value);
+   };
+
+   let editablePlayerName = <span className="player-name">{playerName} </span>;
    let btnCaption = "Edit";
 
    if (isEditing) {
-      playerName = <input type="text" required value={name}></input>;
+      editablePlayerName = (
+         <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+         ></input>
+      );
       btnCaption = "Save";
    }
 
    return (
       <li>
          <span className="player">
-            {playerName}
+            {editablePlayerName}
             <span className="player-symbol">{symbol}</span>
          </span>
          <button onClick={handleEditClick}>{btnCaption}</button>
