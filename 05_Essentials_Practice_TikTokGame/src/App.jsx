88993fbf9dcaa8d0318b5104c6fd552fc76033bd
Zekai Koç <zekaikoc@gmail.com>
2023-12-01 16:40:04 +0100

    game board moved from GameBoard to App

--- 05_Essentials_Practice_TikTokGame/src/App.jsx
+++ 05_Essentials_Practice_TikTokGame/src/App.jsx
@@ -4,6 +4,12 @@ import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
 
+const initialGameBoard = [
+   [null, null, null],
+   [null, null, null],
+   [null, null, null],
+];
+
 function deriveActivePlayer(gameTurns) {
    let currentPlayer = "X";
    if (gameTurns.length > 0 && gameTurns[0].player === "X") {
@@ -14,9 +20,22 @@ function deriveActivePlayer(gameTurns) {
 
 function App() {
    const [gameTurns, setGameTurns] = useState([]);
+   // const [hasWinner, setHasWinner] = useState(false);
    // const [activePlayer, setActivePlayer] = useState("X");
 
    const activePlayer = deriveActivePlayer(gameTurns);
+
+   let gameBoard = initialGameBoard;
+
+   for (const turn of gameTurns) {
+      const { square, player } = turn;
+      const { row, col } = square;
+
+      gameBoard[row][col] = player;
+   }
+
+   for (const combination of WINNING_COMBINATIONS) {
+   }
 
    const handleSelectSquare = (rowIndex, colIndex) => {
       // setActivePlayer((curActivePlayer) => {
@@ -48,7 +67,7 @@ function App() {
                />
             </ol>
 
-            <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+            <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
          </div>
          <Log turns={gameTurns} />
       </main>
