faaf5ee63704dc390e0e4e2fb29a2d9f6edbd4dd
Zekai Koç <zekaikoc@gmail.com>
2023-12-01 16:56:40 +0100

    gameover component added.

--- 05_Essentials_Practice_TikTokGame/src/App.jsx
+++ 05_Essentials_Practice_TikTokGame/src/App.jsx
@@ -3,6 +3,7 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
+import GameOver from "./components/GameOver";
 
 const initialGameBoard = [
    [null, null, null],
@@ -52,6 +53,8 @@ function App() {
       }
    }
 
+   const hasDraw = gameTurns.length === 9 && !winner;
+
    const handleSelectSquare = (rowIndex, colIndex) => {
       setGameTurns((prevTurns) => {
          const currentPlayer = deriveActivePlayer(prevTurns);
@@ -78,7 +81,7 @@ function App() {
                   isActive={activePlayer === "O"}
                />
             </ol>
-            {winner && <p>You won, {winner}!</p>}
+            {(winner || hasDraw) && <GameOver winner={winner} />}
             <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
          </div>
          <Log turns={gameTurns} />
