6b5c196db7a55591e8a003c5871842a33a172482
Zekai Koç <zekaikoc@gmail.com>
2023-12-02 15:47:20 +0100

    app component updated.

--- 05_Essentials_Investment_Calculator/src/components/Header.jsx
+++ 05_Essentials_Investment_Calculator/src/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import logo from "../assets/investment-calculator-logo.png";
+import UserInput from "./UserInput";
 
 const Header = () => {
    return (
-      <header id="header">
-         <img src={logo} alt="Logo showing a money bag." />
-         <h1>Investment Calculator</h1>
-      </header>
+      <>
+         <header id="header">
+            <img src={logo} alt="Logo showing a money bag." />
+            <h1>Investment Calculator</h1>
+         </header>
+         <UserInput />
+      </>
    );
 };
 
