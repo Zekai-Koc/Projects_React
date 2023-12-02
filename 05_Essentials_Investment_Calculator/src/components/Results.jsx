ca0e85a3c7617a87482695dd353ca2f2f1ccce0f
Zekai Koç <zekaikoc@gmail.com>
2023-12-02 15:58:15 +0100

    results component added.

---
+++ 05_Essentials_Investment_Calculator/src/components/Results.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Results = ({ userInput }) => {
+   console.log(userInput);
+   return <p>Results.</p>;
+};
+
+export default Results;
--- 05_Essentials_Investment_Calculator/src/components/UserInput.jsx
+++ 05_Essentials_Investment_Calculator/src/components/UserInput.jsx
@@ -1,22 +1,6 @@
 import React, { useState } from "react";
 
-const UserInput = () => {
-   const [userInput, setUserInput] = useState({
-      initialInvestment: 1000,
-      annualInvestment: 1200,
-      expectedReturn: 6,
-      duration: 10,
-   });
-
-   const handleChange = (inputIdentifier, newValue) => {
-      setUserInput((prevUserInput) => {
-         return {
-            ...prevUserInput,
-            [inputIdentifier]: newValue,
-         };
-      });
-   };
-
+const UserInput = ({ onChange, userInput }) => {
    return (
       <section id="user-input">
          <div className="input-group">
@@ -27,7 +11,7 @@ const UserInput = () => {
                   required
                   value={userInput.initialInvestment}
                   onChange={(e) =>
-                     handleChange("initialInvestment", e.target.value)
+                     onChange("initialInvestment", e.target.value)
                   }
                />
             </p>
@@ -38,9 +22,7 @@ const UserInput = () => {
                   type="number"
                   required
                   value={userInput.annualInvestment}
-                  onChange={(e) =>
-                     handleChange("annualInvestment", e.target.value)
-                  }
+                  onChange={(e) => onChange("annualInvestment", e.target.value)}
                />
             </p>
          </div>
@@ -52,9 +34,7 @@ const UserInput = () => {
                   type="number"
                   required
                   value={userInput.expectedReturn}
-                  onChange={(e) =>
-                     handleChange("expectedReturn", e.target.value)
-                  }
+                  onChange={(e) => onChange("expectedReturn", e.target.value)}
                />
             </p>
 
@@ -64,7 +44,7 @@ const UserInput = () => {
                   type="number"
                   required
                   value={userInput.duration}
-                  onChange={(e) => handleChange("duration", e.target.value)}
+                  onChange={(e) => onChange("duration", e.target.value)}
                />
             </p>
          </div>
