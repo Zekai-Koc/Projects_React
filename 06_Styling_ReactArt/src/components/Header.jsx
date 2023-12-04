59d2593a9c393d872aa329f4be3413794c955b1e
Zekai Koç <zekaikoc@gmail.com>
2023-12-04 14:31:23 +0100

    styling with modules done.

--- 06_Styling_ReactArt/src/components/Header.jsx
+++ 06_Styling_ReactArt/src/components/Header.jsx
@@ -1,17 +1,12 @@
 import logo from "../assets/logo.png";
-import "./Header.css";
+import everything from "./Header.module.css";
 
 export default function Header() {
    return (
       <header>
          <img src={logo} alt="A canvas" />
          <h1>ReactArt</h1>
-         <p
-            style={{
-               color: "red",
-               border: "3px solid black",
-            }}
-         >
+         <p className={everything.para}>
             A community of artists and art-lovers.
          </p>
       </header>
