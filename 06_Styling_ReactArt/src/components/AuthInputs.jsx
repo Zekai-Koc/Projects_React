3e1fcc635b0cb69d4b68612d76eb8d6341f7b2e8
Zekai Koç <zekaikoc@gmail.com>
2023-12-07 06:55:04 +0100

    styled input  added.

--- 06_Styling_ReactArt/src/components/AuthInputs.jsx
+++ 06_Styling_ReactArt/src/components/AuthInputs.jsx
@@ -18,6 +18,16 @@ const Label = styled.label`
    color: #6b7280;
 `;
 
+const Input = styled.input`
+   width: 100%;
+   padding: 0.75rem 1rem;
+   line-height: 1.5;
+   background-color: #d1d5db;
+   color: #374151;
+   border: 1px solid transparent;
+   border-radius: 0.25rem;
+   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
 export default function AuthInputs() {
    const [enteredEmail, setEnteredEmail] = useState("");
    const [enteredPassword, setEnteredPassword] = useState("");
@@ -45,7 +55,7 @@ export default function AuthInputs() {
                <Label className={`label ${emailNotValid ? "invalid" : ""} `}>
                   Email
                </Label>
-               <input
+               <Input
                   type="email"
                   // className={emailNotValid ? "invalid" : undefined}
                   style={{
@@ -58,7 +68,7 @@ export default function AuthInputs() {
             </p>
             <p>
                <Label>Password</Label>
-               <input
+               <Input
                   type="password"
                   className={passwordNotValid ? "invalid" : undefined}
                   onChange={(event) =>
