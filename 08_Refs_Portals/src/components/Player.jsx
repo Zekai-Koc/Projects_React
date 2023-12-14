2c74924ab4f73fa90e8aea642ee0e51333b5f3c2
Zekai Koç <zekaikoc@gmail.com>
2023-12-14 01:04:28 +0100

    name field funcionality done.

--- 08_Refs_Portals/src/components/Player.jsx
+++ 08_Refs_Portals/src/components/Player.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+   const [userName, setuserName] = useState("");
+   const [submitted, setSubmitted] = useState(false);
+
+   const handleChange = (e) => {
+      setSubmitted(false);
+      setuserName(e.target.value);
+   };
+
+   const handleClick = () => {
+      setSubmitted(true);
+   };
+
+   return (
+      <section id="player">
+         <h2>Welcome {submitted ? userName : "unknown entity"}</h2>
+         <p>
+            <input
+               type="text"
+               value={userName}
+               onChange={handleChange}
+               placeholder="Enter your name."
+            />
+            <button onClick={handleClick}>Set Name</button>
+         </p>
+      </section>
+   );
 }
